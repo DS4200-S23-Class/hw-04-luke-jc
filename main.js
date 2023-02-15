86b44609598e68861d5679a1920f98afee69b9ff
Luke Abbatessa <abbatessa.l@northeastern.edu>
2023-02-15 10:44:52 -0500

    Figured out user-defined points

--- main.js
+++ main.js
@@ -6,13 +6,13 @@
 // function to add and remove border on click of a point
 function borderClick(ptID) {
 
-	console.log('clicked' + ptID);
+	console.log("clicked" + ptID);
 
 	let element = document.getElementById(ptID);
 
 	// display the latest selected point in the right hand column
 	let newText = ptID
-	let coords = document.getElementById('coord-list');
+	let coords = document.getElementById("coord-list");
 
 	coords.innerHTML = newText;
 
@@ -28,30 +28,24 @@ function borderClick(ptID) {
 }
 
 function pointClick() {
-	let svgns = "http://www.w3.org/2000/svg", 
-		container = document.getElementById("cont");
+	let xcoords = document.getElementById("x-coords");
+	let ycoords = document.getElementById("y-coords");
+	let xcoord = Number(xcoords.options[xcoords.selectedIndex].text);
+	let ycoord = Number(ycoords.options[ycoords.selectedIndex].text);
 
-	let xcoords = document.getElementsByName("x-coords");
-	let ycoords = document.getElementsByName("y-coords");
+	let newptID = "(" + xcoord + ", " + ycoord + ")"
 
-	for (let i = 0; i < xcoords.length; i++) {
-		for (let j = 0; j < ycoords.length; j++) {
-			if (xcoords[i].checked && ycoords[j].checked) {
-
-				console.log(xcoords[i].value);
-				console.log(ycoords[j].value);
-
-				let circle = document.createElementNS(svgns, 'circle');
-				// circle.setAttributeNS(null, 'class', 'point');
-				// circle.setAttributeNS(null, 'id', '(' + String(vals[i].value) + ', ' + String(vals[j].value) + ')');
-				circle.setAttributeNS(null, 'cx', (xcoords[i].value * 20));
-				circle.setAttributeNS(null, 'cy', ((ycoords[j].value * -20) + 200));
-				circle.setAttributeNS(null, 'r', 5);
-				// circle.setAttributeNS(null, 'onclick', borderClick('(' + String(vals[i].value) + ', ' + String(vals[j].value) + ')'));
-				container.appendChild(circle);
-			}
-		}
-	}
+	let container = document.getElementById("cont");
+	
+	let circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	circle.setAttribute("class", "point");
+	circle.setAttribute("id", newptID);
+	circle.setAttribute("cx", xcoord * 20);
+	circle.setAttribute("cy", ycoord * -20 + 200);
+	circle.setAttribute("r", 5);
+	circle.setAttribute("onclick", "borderClick('" + newptID + "')");
+	
+	container.appendChild(circle);
 }
 
-document.getElementById("subButton").addEventListener('click', pointClick);
+document.getElementById("subButton").addEventListener("click", pointClick);
