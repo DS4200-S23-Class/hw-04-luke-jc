07b369a8901b0c1c5f1912c37bef4b8b50d3bdb7
macintosh air <macintoshair@MacBook-Pro.local>
2023-02-14 20:42:01 -0500

    display coord

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 
+// JS File for Homework 4: Event Handling
+// Luke Abbatessa and Jocelyn Ju
+// Last Modified: 01.15.2023
 
 
-
+// function to add and remove border on click of a point
 function borderClick(ptID) {
-	// upon clicking a point, it will get a border
-	// coordinates should show in right column
-	// if it already has a border, disappear and remove coordinates
-	console.log('click');
+
+	console.log('clicked' + ptID);
 
 	let element = document.getElementById(ptID);
 
+	// display the latest selected point in the right hand column
+	let newText = ptID
+	let coords = document.getElementById('coord-list');
 
+	coords.innerHTML = newText;
+
+	// upon clicking a point, it will get a border
+	// coordinates should show in right column
+	// if it already has a border, disappear and remove coordinates
 	if (element.classList.contains("stroke")) {
 		element.classList.remove("stroke")
 	}
@@ -18,4 +27,3 @@ function borderClick(ptID) {
 		element.classList.add("stroke")
 	}
 }
-
